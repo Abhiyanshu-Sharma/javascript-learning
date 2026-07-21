388c67469d2e1f5c6f813541cdb85d91ac330e6b
Abhiyanshu-Sharma <abhiyanshusharma010305@gmail.com>
2026-07-22 02:05:55 +0530

    Completed Promises

--- Part 3/asynchronous-javascript/132.js
+++ Part 3/asynchronous-javascript/132.js
@@ -1,10 +1,28 @@
 // understand callbacks
 
-function myFunc(callback) {
-    console.log("Function is doing task 1");
-    callback();
+// function myFunc(callback) {
+//     console.log("Function is doing task 1");
+//     callback();
+// }
+
+//  myFunc(() => {
+//   console.log("function is doing task 2");
+// });
+
+function getTwoNumbersAndAdd(number1, number2, onSuccess, onFailure) {
+  if (typeof number1 === "number" && typeof number2 === "number") {
+    onSuccess(number1, number2);
+  } else {
+    onFailure();
+  }
 }
 
- myFunc(() => {
-  console.log("function is doing task 2");
-});
+function addTwoNumbers(num1, num2) {
+  console.log(num1 + num2);
+}
+
+function onFail(){
+    console.log("Wrong data type");
+    console.log("please pass numbers only")
+}
+getTwoNumbersAndAdd(4, 4,addTwoNumbers, onFail);
